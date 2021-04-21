596c98bd0d5a2dc886af7c63b93ea2c99a59bd68
patricio1984 <patricio1984@gmail.com>
2021-04-21 18:24:54 -0300

    Se agregó activeclassname en header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ModelFooter from "./components/Models/ModelFooter";
 import FileModel from "./components/FileModel/FileModel";
 import Navbar from "./components/Nav/Navbar";
 import Models from "./components/Models/Models";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, NavLink, Redirect } from 'react-router-dom';
 import Logo from "./assets/img/logo.png";
 import Spinner from "./components/Spinner";
 
@@ -32,8 +32,8 @@ function App() {
                 <img src={Logo} className="logo" alt="Ego Logo" />
               </Link>
                   <div className="menu-container">
-                      <Link to={'/models'} className="menu-items">Modelos</Link>
-                      <Link to={`/models/:id`} className="menu-items">Ficha del Modelo</Link>
+                      <NavLink exact to={'/models'} className="menu-items" activeClassName="menu-items-active">Modelos</NavLink>
+                      <NavLink exact to={`/models/:id`} className="menu-items" activeClassName="menu-items-active">Ficha del Modelo</NavLink>
                   </div>
                   <div className="bottom-line"></div>             
               </nav>
